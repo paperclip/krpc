a465146b5a65d7a858280a74e6a93144be505bb0
djungelorm <djungelorm@users.noreply.github.com>
2014-04-03 19:56:40 +0100

    Write to logs using UnityEngine.Debug.Log if assembly is loaded

--- src/kRPC/Utils/Logger.cs
+++ src/kRPC/Utils/Logger.cs
@@ -3,11 +3,26 @@ using UnityEngine;
 
 namespace KRPC.Utils
 {
-    static class Logger
+    public static class Logger
     {
+        private static bool shouldLogToUnity = false;
+
+        public static bool ShouldLogToUnity {
+            private get { return shouldLogToUnity; }
+            set { shouldLogToUnity = value; }
+        }
+
         internal static void WriteLine (string line)
         {
-            Console.WriteLine ("[kRPC] " + line);
+            if (ShouldLogToUnity)
+                WriteLineToUnityLog ("[kRPC] " + line);
+            else
+                Console.WriteLine ("[kRPC] " + line);
+        }
+
+        private static void WriteLineToUnityLog (string line)
+        {
+            Debug.Log (line);
         }
     }
 }
--- test/TestServer/Program.cs
+++ test/TestServer/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using KRPC;
+using KRPC.Utils;
 
 namespace TestServer
 {
@@ -8,6 +9,7 @@ namespace TestServer
     {
         public static void Main (string[] args)
         {
+            KRPC.Utils.Logger.ShouldLogToUnity = false;
             var server = new KRPCServer (IPAddress.Loopback, ushort.Parse (args [0]));
             var timeSpan = new TimeSpan ();
             server.GetUniversalTime = () => timeSpan.TotalSeconds;
